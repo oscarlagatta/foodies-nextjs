1ef8d01d8989db82ab3f7ed21f915f0f480cd95f
Oscar <oscarlagatta@gmail.com>
2024-05-12 11:45:19 +0100

    Add meal sharing feature and necessary libraries
    The commit introduces a new feature to share meals which involves the creation of the lib/actions.js and the update of lib/meals.js to include a new function 'saveMeal'. This function saves meals to the database and writes images to the file system. Necessary libraries are also added to the package.json file like 'slugify', 'xss' and applying the appropriate changes in package-lock.json file. The UI updates corresponding to the new functionalities are also reflected in app/meals/share/page.js.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,33 +1,8 @@
 import classes from './page.module.css';
 import ImagePicker from "@/app/meals/image-picker";
+import {shareMeal} from "@/lib/actions";
 
 export default function ShareMealPage() {
-
-    async function shareMeal(formData) {
-        'use server';
-
-        // creates a server action, to execute only in the server,
-        // this is a function that only executes in the server, so
-        // we need to explicitly say 'user server'. but we must add the async to the function.
-
-        // this feature exists because we take the server action and assign to the action prop of the form.
-
-        // handle submitted data
-        const meal = {
-            title: formData.get('title'),
-            summary: formData.get('summary'),
-            instructions: formData.get('instructions'),
-            image: formData.get('image'),
-            creator: formData.get('name'),
-            creator_email: formData.get('email')
-        }
-
-        // store in the database, but the image to the file system and path to the database
-        console.log(meal);
-
-    }
-
-
     return (
         <>
             <header className={classes.header}>
@@ -41,20 +16,20 @@ export default function ShareMealPage() {
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
-                            <input type="text" id="name" name="name" required />
+                            <input type="text" id="name" name="name" required/>
                         </p>
                         <p>
                             <label htmlFor="email">Your email</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" required/>
                         </p>
                     </div>
                     <p>
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" name="title" required />
+                        <input type="text" id="title" name="title" required/>
                     </p>
                     <p>
                         <label htmlFor="summary">Short Summary</label>
-                        <input type="text" id="summary" name="summary" required />
+                        <input type="text" id="summary" name="summary" required/>
                     </p>
                     <p>
                         <label htmlFor="instructions">Instructions</label>
@@ -65,7 +40,7 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker label='Your image' name='image' />
+                    <ImagePicker label='Your image' name='image'/>
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
