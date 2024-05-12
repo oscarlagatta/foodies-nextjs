ad8e973fb1abec25f2a289c15df1539a2caad5f3
Oscar <oscarlagatta@gmail.com>
2024-05-12 09:05:02 +0100

    Add meals page and database logic
    This commit introduces the meals page and related database logic. It includes the `meals-grid.js` component to display meals in a grid layout, `initdb.js` to initialize and populate the SQLite database, and `meals.js` to fetch the meals data. The CSS for the meals page and grid component have also been added. Additionally, the `package.json` file has been updated to include the `better-sqlite3` and `sqlite3` packages.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,29 @@
-export default function Meals() {
+import styles from './page.module.css';
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from "@/lib/meals";
+
+
+export default async function MealsPage() {
+
+
+    const meals = await getMeals();
+
+
     return (
-        <h1>meals page</h1>
+        <>
+            <header className={styles.header}>
+                <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
+                <p>Choose your favorite recipe and cook it yourself, it's easy and fun.</p>
+                <p className={styles.cta}>
+                    <Link href='/meals/share'>
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
     )
 }
