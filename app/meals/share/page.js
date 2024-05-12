d725553897d5f055efcd167a26038aef7e6f63dd
Oscar <oscarlagatta@gmail.com>
2024-05-12 15:52:08 +0100

    Add input validation for sharing a meal and update dependencies
    The changes in the code include adding validation for meal sharing inputs. If the inputs are invalid, a message will be returned to the user. Dependent packages in the application have also been updated to their latest versions.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,16 @@
+'use client';
+
 import classes from './page.module.css';
 import ImagePicker from "@/app/meals/image-picker";
 import {shareMeal} from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+
+    const [state, formAction] = useFormState(shareMeal, {message: null});
+
     return (
         <>
             <header className={classes.header}>
@@ -12,7 +20,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -41,8 +49,10 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label='Your image' name='image'/>
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        {/*<button type="submit">Share Meal</button>*/}
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
