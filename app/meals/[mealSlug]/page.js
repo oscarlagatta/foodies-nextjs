16d34f4dc1b96b012499bf37e2d8cc1d675541af
Oscar <oscarlagatta@gmail.com>
2024-05-12 09:54:45 +0100

    Implement detailed meal information page
    Enhanced the meal information page to display detailed information of individual meals. Changes include querying meal data based on the slug parameter and rendering the meal details dynamically on the page. An associated CSS file was also created for the necessary stylings of the page.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,36 @@
-export default function  MealsDetailPage() {
+import styles from './page.module.css'
+import Image from "next/image";
+import {getMeal} from "@/lib/meals";
+export default function  MealsDetailPage({params}) {
+
+    const meal = getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return (
-        <h1>Meals Page</h1>
+        <>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={styles.summary}>{meal.summary}</p>
+                </div>
+            </header>
+
+            <main>
+
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }}>
+
+                </p>
+
+            </main>
+
+        </>
     )
 }
