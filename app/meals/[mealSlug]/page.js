a953fd0ca79aa528a446a618daff66f737d42b8b
Oscar <oscarlagatta@gmail.com>
2024-05-12 09:57:01 +0100

    Add NotFound handling for invalid meal requests
    This update includes the creation of a NotFound.js file and adding a check in the 'mealsDetailPage' file. This ensures that a user-friendly 'Not Found' page is displayed, specifically when invalid meal data has been requested, improving overall error handling in the meals section of the app.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import styles from './page.module.css'
 import Image from "next/image";
 import {getMeal} from "@/lib/meals";
+import {notFound} from "next/navigation";
 export default function  MealsDetailPage({params}) {
 
     const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
 
     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return (
